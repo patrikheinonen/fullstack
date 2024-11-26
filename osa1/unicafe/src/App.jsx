39ef55d6efd8fc8c67a408b10b8acf8237096c21
Patrik Heinonen <patrik.heinonen@oivan.com>
2024-11-26 22:05:43 +0200

    osa 1 kaikki tehtävät tehty. osa1/part1 kansiossa myös kursilla käytyä react koodia

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const calculateAverageScore = () => {
+    return (good - bad) / (bad + good + neutral);
+  };
+
+  const percentageOfGoodScores = () => {
+    return (good / (bad + good + neutral)) * 100;
+  };
+
+  if (good == 0 && neutral == 0 && bad == 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={bad + good + neutral} />
+          <StatisticLine text="average" value={calculateAverageScore()} />
+          <StatisticLine text="positive" value={percentageOfGoodScores()} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
